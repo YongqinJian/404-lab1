939dbcf1e9b0d0177f3ef9753932022bac9ad501
Yongqin Jian <32945661+YongqinJian@users.noreply.github.com>
2021-01-13 18:36:48 -0700

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -1,5 +1,8 @@
 import requests
 
-print(requests.__version__)
+#print(requests.__version__)
 
 home_page = requests.get('http://google.com/')
+
+my_script = requests.get('https://raw.githubusercontent.com/YongqinJian/404-lab1/main/lab1.py')
+print(my_script.text)
